ec72209ea54d252f799dc81793b863dd966a6668
shimamura <2011140075w@ed.fuk.kindai.ac.jp>
2022-11-16 06:46:47 +0000

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_02_080216) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
+ActiveRecord::Schema.define(version: 2022_11_02_081338) do
 
   create_table "tracks", force: :cascade do |t|
     t.string "title"
@@ -28,6 +19,15 @@ ActiveRecord::Schema.define(version: 2022_11_02_080216) do
     t.binary "thumbnail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
