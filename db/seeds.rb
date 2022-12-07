6b5f7809b44f91836730a78a1047c2e435b6aee6
shimamura <2011140075w@ed.fuk.kindai.ac.jp>
2022-12-07 09:19:15 +0000

    set demodata

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+yamada = User.create!(name: '山田太郎', 
+                      email: 'yamada@example.com', 
+                      password_digest:  BCrypt::Password.create('yamada'))
+tanaka = User.create!(name: '田中次郎', 
+                      email: 'tanaka@example.com', 
+                      password_digest:  BCrypt::Password.create('tanaka'))
+
+User.all.each do |user|
+  name = user.email.split("@")[0]
+  1.upto(5) do |i|
+    title = "楽曲_#{name}_#{i}"
+    Track.create!(user: user, 
+                  title: title, 
+                  mp3: File.open("./db/seed_data/music/#{name}.mp3"), 
+                  description: "楽曲の説明楽曲の説明楽曲の説明楽曲の説明楽曲の説明楽曲の説明楽曲の説明楽曲の説明楽曲の説明楽曲の説明", 
+                  thumbnail: File.open("./db/seed_data/image/#{title}.jpg").read)
+  end
+  
+  1.upto(2) do |i|
+    title = "プレイリスト_#{name}_#{i}"
+    playlist = Playlist.create!(user: user, 
+                                title: title,
+                                thumbnail: File.open("./db/seed_data/image/#{title}.jpg").read)
+  end
+end
+
+1.upto(3) do |m|
+  Playlist.first.tracks << Track.find(m)
+end
