1b4f30ea9d97afac1ca8fc57861ae7138af6032c
03Daiba <ubuntu@ip-172-31-83-240.ec2.internal>
2022-12-14 06:28:46 +0000

    fix playlist bug

--- app/models/belonging.rb
+++ app/models/belonging.rb
@@ -2,5 +2,7 @@ class Belonging < ApplicationRecord
     belongs_to :track
     belongs_to :playlist
     
-    validates :track, presence: true, uniqueness: true
+    validates :track, presence: true
+    #validates :track, presence: true, uniqueness: true
+    #validates :playlist, uniqueness: true
 end
