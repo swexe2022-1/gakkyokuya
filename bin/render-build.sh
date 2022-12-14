b76f77b58165b34b8610a6b7103900da155ced25
shimamura <2011140075w@ed.fuk.kindai.ac.jp>
2022-12-14 07:04:30 +0000

    fix: render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,6 @@ bundle install
 yarn install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-budnle exec rake db:drop
+bundle exec rake db:drop
 bundle exec rake db:migrate
 bundle exec rake db:seed
